c8922f9f9de3d944a09f04f84b92cb91ebf9303f
mister.San4es <mister.San4es@list.ru>
2022-07-04 17:50:12 +0400

    added style catalog and slider

--- src/index.js
+++ src/index.js
@@ -2,3 +2,25 @@ import './index.html';
 import './cart.html';
 import './card.html';
 import './index.scss';
+
+  import Swiper, {Thumbs} from 'swiper';
+  import 'swiper/css';
+
+  const thumbSwiper =  new Swiper('.card__slider-thumb', {
+    spaceBetween: 44,
+    slidesPerView: 3,
+  });
+
+  new Swiper('.card__image', {
+    spaceBetween: 10,
+    slidesPerView: 1,
+    thumbs: {
+      swiper: thumbSwiper,
+    },
+    modules: [Thumbs]
+  });
+ 
+  new Swiper('.recommended__slider', {
+    spaceBetween: 30,
+    slidesPerView: 5,
+  });
