3cfc58e06fb6ab5b047f8347a8ab6ba819cbbc20
mister.San4es <mister.San4es@list.ru>
2022-07-06 13:35:29 +0400

    added pagination

--- src/index.js
+++ src/index.js
@@ -3,24 +3,36 @@ import './cart.html';
 import './card.html';
 import './index.scss';
 
-  import Swiper, {Thumbs} from 'swiper';
-  import 'swiper/css';
+import Swiper, {Thumbs} from 'swiper';
+import 'swiper/css';
+import { pagination } from './modules/pagination';
 
-  const thumbSwiper =  new Swiper('.card__slider-thumb', {
-    spaceBetween: 44,
-    slidesPerView: 3,
-  });
+const paginationWrapper = document.querySelector('.pagination');
+const pageURL = new URL(location);
+const page = +pageURL.searchParams.get('page') || 1;
 
-  new Swiper('.card__image', {
-    spaceBetween: 10,
-    slidesPerView: 1,
-    thumbs: {
-      swiper: thumbSwiper,
-    },
-    modules: [Thumbs]
-  });
- 
-  new Swiper('.recommended__slider', {
-    spaceBetween: 30,
-    slidesPerView: 5,
-  });
+try {
+  pagination(paginationWrapper, 20, page, 5);
+} catch (e) {
+  console.warn(e);
+  console.warn('Это не главная страница');
+}
+
+const thumbSwiper =  new Swiper('.card__slider-thumb', {
+  spaceBetween: 44,
+  slidesPerView: 3,
+});
+
+new Swiper('.card__image', {
+  spaceBetween: 10,
+  slidesPerView: 1,
+  thumbs: {
+    swiper: thumbSwiper,
+  },
+  modules: [Thumbs]
+});
+
+new Swiper('.recommended__slider', {
+  spaceBetween: 30,
+  slidesPerView: 5,
+});
