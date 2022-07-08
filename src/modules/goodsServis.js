ef1b0c7a1d91ef0313ca1c63fc58553bf58a9ddd
mister.San4es <mister.San4es@list.ru>
2022-07-08 15:45:33 +0400

    error

--- src/modules/goodsServis.js
+++ src/modules/goodsServis.js
@@ -1,9 +1,13 @@
 import { API_URI } from "./var";
 
-export const getGoods = ({page}) => {
-  const url = new URL(`${API_URI}api/goods`)
+export const getGoods = ({ page }) => {
+  const url = new URL(`${API_URI}api/goods`);
 
-  if (page) url.searchParams.append('page', page);
+  if (page) url.searchParams.append("page", page);
 
-  return fetch(url).then(response => response.json())
-}
+  return fetch(url).then((response) => response.json());
+};
+
+export const getGoodsItem = (id) => 
+  fetch(`${API_URI}api/goods/${id}`)
+    .then((response) => response.json());
---
+++ src/modules/renderItem.js
@@ -0,0 +1,22 @@
+const createCardImageSlider = (largeImages) => {
+  const cardImageSlider = document.createElement('ul');
+  cardImageSlider.className = 'swiper-wrapper';
+
+  const cardImageSlides = largeImages.map(url => {
+    const li = document.createElement('li');
+    li.className = 'swiper-slide';
+    const img = new Image();
+    img.src = `${API_URI}${url}`;
+    li.append(img);
+    return li;
+  })
+
+  cardImageSlider.append(...cardImageSlides);
+  return cardImageSlider;
+}
+
+export const renderItem = item => {
+  console.log(item);
+  const cardImage = document.querySelector('.card__image');
+  cardImage.append(createCardImageSlider(item.images.large))
+}
